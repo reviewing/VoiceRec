69f734ba50ade748ed1c3c34be3a67198f46fbd3
Hong Duan <duanhong169@gmail.com>
2013-11-03 15:00:41 +0800

    基本语音识别功能实现

--- HongVoiceRec/Controller/HVRSingleSentenceNoUIViewController.m
+++ HongVoiceRec/Controller/HVRSingleSentenceNoUIViewController.m
@@ -8,6 +8,9 @@
 
 #import "HVRSingleSentenceNoUIViewController.h"
 
+#define API_KEY @"LjfQaGXUdnHHGZFBReQBtVGs"
+#define SECRET_KEY @"awDw9VbFAAbFFKWDhcthn7MrZv44FQI0"
+
 @interface HVRSingleSentenceNoUIViewController ()
 {
     BOOL isSpeaking;
@@ -19,8 +22,6 @@
 
 - (IBAction)tapToSpeak:(id)sender;
 - (IBAction)cancelRec:(id)sender;
-
-@property (strong, nonatomic) BDVoiceRecognitionClient *voiceRecClient;
 
 @end
 
@@ -53,9 +54,44 @@
     self.cancelRecButton.enabled = isSpeaking;
 }
 
+- (void)setResultText:(NSString *)text
+{
+    self.resultLabel.text = text;
+    [self.resultLabel sizeToFit];
+    
+    // Resize the frame's width to 280 (320 - margins) see: http://stackoverflow.com/questions/1054558/vertically-align-text-within-a-uilabel
+    CGRect myFrame = self.resultLabel.frame;
+    myFrame = CGRectMake(myFrame.origin.x, myFrame.origin.y, 280, myFrame.size.height);
+    self.resultLabel.frame = myFrame;
+}
+
 - (IBAction)tapToSpeak:(id)sender {
     if (isSpeaking) {
+        [[BDVoiceRecognitionClient sharedInstance] speakFinish];
+    }
+    else {
+        // 设置开发者信息
+        [[BDVoiceRecognitionClient sharedInstance] setApiKey:API_KEY withSecretKey:SECRET_KEY];
         
+        // 设置语音识别模式，默认是搜索模式, 请在设置服务器地址之前进行设置，避免服务器地址设置出错
+        [[BDVoiceRecognitionClient sharedInstance] setCurrentVoiceRecognitionMode:EVoiceRecognitionModeSearch];
+        
+        // 开始监听音量
+        [[BDVoiceRecognitionClient sharedInstance] listenCurrentDBLevelMeter];
+        
+        // 开始说话提示音
+        [[BDVoiceRecognitionClient sharedInstance] setPlayTone:EVoiceRecognitionPlayTonesRecStart isPlay:YES];
+        // 结束说话提示音
+        [[BDVoiceRecognitionClient sharedInstance] setPlayTone:EVoiceRecognitionPlayTonesRecEnd isPlay:YES];
+        
+        int startStatus = -1;
+        startStatus = [[BDVoiceRecognitionClient sharedInstance] startVoiceRecognition:self];
+        // 启动失败，报告错误
+        if (startStatus != EVoiceRecognitionStartWorking)
+        {
+            [self setResultText:[NSString stringWithFormat:@"启动失败，错误码：%d", startStatus]];
+            return;
+        }
     }
     
     isSpeaking = !isSpeaking;
@@ -63,10 +99,33 @@
 }
 
 - (IBAction)cancelRec:(id)sender {
+    [[BDVoiceRecognitionClient sharedInstance] stopVoiceRecognition];
 }
 
 - (void)VoiceRecognitionClientWorkStatus:(int) aStatus obj:(id)aObj
 {
-    
+    switch (aStatus) {
+        case EVoiceRecognitionClientWorkStatusFinish:
+        {
+            self.resultLabel.text = nil;
+            
+            // 搜索模式下的结果为数组，示例为
+            // ["公园", "公元"]
+            NSMutableArray *audioResultData = (NSMutableArray *)aObj;
+            NSMutableString *tmpString = [[NSMutableString alloc] initWithString:@""];
+            
+            for (int i=0; i < [audioResultData count]; i++)
+            {
+                [tmpString appendFormat:@"%@\r\n",[audioResultData objectAtIndex:i]];
+            }
+            
+            [self setResultText:tmpString];
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
 }
 @end
