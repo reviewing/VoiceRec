8129a8b8e43ed149682081aa4337404a1c2cc6b9
Hong Duan <duanhong169@gmail.com>
2013-11-03 12:30:25 +0800

    准备不使用识别控件实现语音识别

---
+++ HongVoiceRec/Controller/HVRSingleSentenceNoUIViewController.h
@@ -0,0 +1,13 @@
+//
+//  HVRSingleSentenceNoUIViewController.h
+//  HongVoiceRec
+//
+//  Created by  段弘 on 13-11-3.
+//  Copyright (c) 2013年 百度. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HVRSingleSentenceNoUIViewController : UIViewController
+
+@end
---
+++ HongVoiceRec/Controller/HVRSingleSentenceNoUIViewController.m
@@ -0,0 +1,29 @@
+//
+//  HVRSingleSentenceNoUIViewController.m
+//  HongVoiceRec
+//
+//  Created by  段弘 on 13-11-3.
+//  Copyright (c) 2013年 百度. All rights reserved.
+//
+
+#import "HVRSingleSentenceNoUIViewController.h"
+
+@interface HVRSingleSentenceNoUIViewController ()
+
+@end
+
+@implementation HVRSingleSentenceNoUIViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
